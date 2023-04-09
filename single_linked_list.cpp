2962cd2108be101f8475d3877f8c942dd5099154
Mansour <mansour24@protonmail.com>
2023-04-09 14:59:29 +0200

    header small typo

--- single_linked_list.cpp
+++ single_linked_list.cpp
@@ -1,6 +1,6 @@
 /*
  * FCAI – Data Structure – 2023 - Assignment 2
- * Program Name: array linked list
+ * Program Name: single linked list
  * Last Modification Date: 9/04/2023
  * Author and ID and Group: Adham Wael Mansour - 20210057 - Group B
  * Teaching Assistant: unknown
